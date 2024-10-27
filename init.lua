65d6a1133aa483a53c305958fb2450f5183e0e6c
tuanp <tuanphu9249@gmail.com>
2024-10-27 10:53:41 +1030

    neogit itergration

--- init.lua
+++ init.lua
@@ -285,6 +285,7 @@ require('lazy').setup({
   require 'custom.plugins.debug',
   require 'custom.plugins.neoscroll',
   require 'custom.plugins.transparent',
+  require 'custom.plugins.neogit',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
